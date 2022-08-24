6187c464e91de949b458c6d064e94a8264db15e5
RootHarrow <root.harrow@gmail.com>
2022-08-24 16:20:01 -0400

    adding python

---
+++ python2.py
@@ -0,0 +1,5 @@
+#here put your name to print your name
+name = "Qasem"
+
+#Here python will print your name 
+print("Welcome "+name+" to your project")
---
+++ python3.py
@@ -0,0 +1,8 @@
+name1= "qasem"
+
+name2= " hasan"
+
+#this if i want to make two names or more in one variable
+names= name1+name2
+
+print(names)
